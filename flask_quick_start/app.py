ecc0eea9e383a1a03e7d2e1428ee6157b4cf75df
Jeff Call <jeff.call.ch@gmail.com>
2020-06-18 15:17:39 +0200

    update

--- flask_quick_start/app.py
+++ flask_quick_start/app.py
@@ -1,25 +1,31 @@
-#Flask hello world!
+# Flask hello world!
 from flask import Flask
 
-#create app object
+# create app object
 app = Flask(__name__)
 
-#use decorator to link to an url
+# error handling
+app.config["DEBUG"] = True
+
+# define the view using a function to return a string
+# use decorator to link to an url
 @app.route("/")
 @app.route("/hello")
-
-#define the view using a function to return a string
+# define the view using a function to return a string
 def hello_world():
-    return "Hello World!"
+    return "Hello World! and"
+
 
 @app.route("/test/<search_query>")
 def search(search_query):
     return search_query
 
+
 @app.route("/integer/<int:value>")
 def int_type(value):
-    print (value + 1)
+    print(value + 1)
     return "correct"
+
 
 @app.route("/name/<name>")
 def index(name):
@@ -29,6 +35,7 @@ def index(name):
     else:
         return "Not found", 404
 
-#start the dev server
+
+# start the dev server
 if __name__ == "main":
     app.run()
